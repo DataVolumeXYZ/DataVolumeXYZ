f5497ccdd2ca0478dc2976274ec6891925c22226
JamesDBartlett3 <37491308+JamesDBartlett3@users.noreply.github.com>
2024-09-02 19:20:05 -0500

    added reading progress bar to posts

--- assets/js/post.js
+++ assets/js/post.js
@@ -1,19 +1,61 @@
-// copy code to clipboard button
-var codeBlocks = document.querySelectorAll('pre.highlight');
+// Copy code to clipboard button
+var codeBlocks = document.querySelectorAll("pre.highlight");
 codeBlocks.forEach(function (codeBlock) {
-	var copyButton = document.createElement('button');
-	copyButton.className = 'copy';
-	copyButton.type = 'button';
-	copyButton.ariaLabel = 'Copy code to clipboard';
-	copyButton.innerText = 'Copy';
-	codeBlock.append(copyButton);
-	copyButton.addEventListener('click', function () {
-		var code = codeBlock.querySelector('code').innerText.trim();
-		window.navigator.clipboard.writeText(code);
-		copyButton.innerText = 'Copied';
-		var fourSeconds = 4000;
-		setTimeout(function () {
-			copyButton.innerText = 'Copy';
-		}, fourSeconds);
+  var copyButton = document.createElement("button");
+  copyButton.className = "copy";
+  copyButton.type = "button";
+  copyButton.ariaLabel = "Copy code to clipboard";
+  copyButton.innerText = "Copy";
+  codeBlock.append(copyButton);
+  copyButton.addEventListener("click", function () {
+    var code = codeBlock.querySelector("code").innerText.trim();
+    window.navigator.clipboard.writeText(code);
+    copyButton.innerText = "Copied";
+    var fourSeconds = 4000;
+    setTimeout(function () {
+      copyButton.innerText = "Copy";
+    }, fourSeconds);
+  });
+});
+
+// Progress bar
+document.addEventListener("DOMContentLoaded", function () {
+	var progressBar = document.querySelector(".progress")
+	var postContent = document.querySelector(".post-content")
+	var documentHeight = Math.max(
+		document.documentElement.clientHeight,
+		document.documentElement.scrollHeight,
+		document.documentElement.offsetHeight,
+		document.body.scrollHeight,
+		document.body.offsetHeight
+	)
+	var postContentHeight = postContent.scrollHeight
+	var bannerHeight = document.querySelector("#main").scrollHeight
+	var siteHeaderHeight = document.querySelector(".site-header").scrollHeight
+	var seriesOverviewHeight = document.querySelector(".series-overview").scrollHeight + 0
+	var topOffset = seriesOverviewHeight + siteHeaderHeight + bannerHeight
+	var bottomOffset = documentHeight - postContentHeight + topOffset - seriesOverviewHeight
+
+	function progressShow() {
+		if (window.scrollY < topOffset) {
+			progressBar.classList.remove("progress--visible");
+			progressBar.classList.add("progress--hidden");
+		} else {
+			progressBar.classList.remove("progress--hidden");
+			progressBar.classList.add("progress--visible");
+		}
+	}
+
+	function progressbarScroll() {
+		var totalScroll = ((window.scrollY - topOffset) / (postContentHeight - topOffset - bottomOffset)) * 100;
+		document.querySelector(".progress__bar").style.width = totalScroll + "%";
+	}
+
+	progressShow();
+	progressbarScroll();
+
+	window.addEventListener("scroll", function () {
+		progressShow();
+		progressbarScroll();
 	});
 });
